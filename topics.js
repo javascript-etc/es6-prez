ef33cba733d8ca9e0acb7405e31d2e952ff69d8d
Joe Sepi <sepi@joesepi.com>
2015-02-12 10:46:30 -0500

    more topics2

--- topics.js
+++ topics.js
@@ -36,4 +36,18 @@ console.log(Nascar.name); // 'Gordon'
 // fat arrow
 
 
+// Template strings are neato
 
+getPath(context, identity, ...props) {
+  let key = util.findByIdentity(this._model.get(`!${context}`), identity);
+  if (!key) { throw new Error('Unable to findByIdentity'); }
+
+  let path = `!${context}.${key}`;
+  props.forEach(function(item) {
+    // OLD WAY: path += '.' + item;
+    path = `${path}.${item}`;
+  });
+
+  console.log(path);
+  return path;
+}
