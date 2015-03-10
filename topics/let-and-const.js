af26e11186c894c6709d22f2400dbf1af28473ba
Joe Sepi <sepi@joesepi.com>
2015-03-09 23:39:41 -0400

    note about the issue I had today

--- topics/let-and-const.js
+++ topics/let-and-const.js
@@ -2,3 +2,6 @@
 
 let something = "something";
 const another = "wont change";
+
+// show the example I just experienced where const was defined in an if and then used later. Babel got tricky
+// great example showing how let/const should work and also showing off Babel
